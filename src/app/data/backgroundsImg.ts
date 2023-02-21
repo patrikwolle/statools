9aa9bb976260fe7f98dd4a04087d015493d0c9bc
Leviasa <annkathrin.froin@gmail.com>
2023-02-21 21:02:23 +0100

    3 new nebula

--- src/app/data/backgroundsImg.ts
+++ src/app/data/backgroundsImg.ts
@@ -4,4 +4,7 @@ export const backgrounds: backgroundInterface[] = [
   { name: 'Space 1', path: 'assets/background/Space1.png' },
   { name: 'Space 2', path: 'assets/background/Space2.png' },
   { name: 'Space 3', path: 'assets/background/Space3.png' },
+  { name: 'Space 4', path: 'assets/background/Space4.png' },
+  { name: 'Space 5', path: 'assets/background/Space5.png' },
+  { name: 'Space 6', path: 'assets/background/Space6.png' },
 ];
