8d461ce068ae6fe1adf9b51500baa0e4e696b10c
Fabian <f.blaschke@vestpoint.de>
2023-03-13 21:59:48 +0100

    adds eyebrows

--- src/app/data/svgArray.ts
+++ src/app/data/svgArray.ts
@@ -370,6 +370,24 @@ export const avatarSvgList: avatarList[] = [
     },
   },
   {
+    file: 'eyebrows_short.svg',
+    tags: {
+      imagePart: imageParts.eyebrows,
+      gender: [gender.male, gender.female],
+      species: [
+        alienSpeciesList.andorian,
+        alienSpeciesList.betazoid,
+        alienSpeciesList.denobulan,
+        alienSpeciesList.ferengi,
+        alienSpeciesList.human,
+        alienSpeciesList.klingon,
+        alienSpeciesList.orion,
+        alienSpeciesList.trill,
+        alienSpeciesList.tellarite,
+      ],
+    },
+  },
+  {
     file: 'eyebrows_klingon_wild.svg',
     tags: {
       imagePart: imageParts.eyebrows,
