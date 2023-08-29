e4a784806a05949ba4f974fb09967eacdaa9e5c2
patrikwolle <patrik.wolle@googlemail.com>
2023-08-29 20:10:14 +0200

    New button

--- src/app/elements/sta-select-button/sta-select-button.component.ts
+++ src/app/elements/sta-select-button/sta-select-button.component.ts
@@ -1,17 +1,50 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
+import {AvatarService} from "../../services/avatar.service";
+import {avatarList} from "../../interfaces/avatar.interface";
+import {HttpClient} from "@angular/common/http";
+import {image} from "html2canvas/dist/types/css/types/image";
 
 @Component({
   selector: 'app-sta-select-button',
   templateUrl: './sta-select-button.component.html',
   styleUrls: ['./sta-select-button.component.scss']
 })
-export class StaSelectButtonComponent {
+export class StaSelectButtonComponent implements OnInit{
 
   @Input('svgPath') pathToSvg: string = '';
   @Input('height') heightValue: string = '300px';
   @Input('width') widthValue: string = '300px';
 
-  constructor() {
+
+  svgInput: SafeHtml = '';
+
+  constructor(
+    private http: HttpClient,
+    private sanitizer: DomSanitizer,
+  ) {
+  }
+
+
+  changeSizeOfSVG(width: string, height: string, input: string): void {
+    this.http.get(
+      input,
+      {
+        responseType: 'text',
+      }
+    ).subscribe((res) => {
+        let image = `${
+          res.split('width')[0]
+        }width="${width}px" height="${height}px" viewbox${
+          res.split(/viewBox(.*)/s)[1]
+        }`
+        this.svgInput =
+          this.sanitizer.bypassSecurityTrustHtml(image         );
+
+  })}
+
+  ngOnInit(): void {
+    this.changeSizeOfSVG('250', '250', 'assets/avatar/uniform/uniform_female1_2370.svg')
   }
 
 
